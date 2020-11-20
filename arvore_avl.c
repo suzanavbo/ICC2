c9eb62c69562c9c948976824d1981a6eb25388c9
Suzana Vilas Boas <39133168+suzanavbo@users.noreply.github.com>
2020-11-20 18:20:48 -0300

    Update arvore_avl.c

--- arvore_avl.c
+++ arvore_avl.c
@@ -148,14 +148,15 @@ int balance(NODE* node){
  *                                 struct após a realização da rotação                                
  */
 NODE* rotate_right(NODE *node){
-	NODE* aux;
+  NODE* aux;
 	
   aux = node->left;
-	node->left = aux->right;
-	aux->right= node;
-	node->height= height_f(node);
-	aux->height = height_f(aux);
-	return aux;
+  node->left = aux->right;
+  aux->right= node;
+  node->height= height_f(node);
+  aux->height = height_f(aux);
+  
+  return aux;
 }
 
 
@@ -168,15 +169,15 @@ NODE* rotate_right(NODE *node){
  *                                 struct após a realização da rotação                                
  */
 NODE* rotate_left(NODE* node){
-	NODE* aux;
+  NODE* aux;
 
-	aux = node->right;
-	node->right = aux->left;
-	aux->left = node;
-	node->height = height_f(node);
-	aux->height = height_f(aux);
-	
-	return aux;
+  aux = node->right;
+  node->right = aux->left;
+  aux->left = node;
+  node->height = height_f(node);
+  aux->height = height_f(aux);
+  	
+  return aux;
 }
 
 
@@ -189,13 +190,14 @@ NODE* rotate_left(NODE* node){
  */
 NODE* newNode(int newValue){
   NODE* aux = (NODE*) malloc(sizeof(NODE)); //Alocação dinâmica de memória. Aloca na
-					                                  //heap o espaço esritamente necessário para a
+					    //heap o espaço esritamente necessário para a
                                             //utilização de um tipo NODE
   //Criação de um nó da árvore binária
   aux->value = newValue;
   aux->left = NULL;
   aux->right = NULL;
   aux->height = 1;
+  
   return aux;
 }
 
@@ -204,7 +206,7 @@ NODE* newNode(int newValue){
  *    
  *    Entradas: - node     ==> endereço do primeiro byte do pirmeiro parâmetro de uma
  *                             struct (tipo) node na qual se pretende fazer a inserção
- *		          - newValue ==> valor a ser incluído na árvore
+ *		- newValue ==> valor a ser incluído na árvore
  *     
  *    Saída:    - node     ==> endereço do primeiro byte do primeiro parâmetro da 
  *                             struct (tipo) node na qual se fez a inserção                              
@@ -249,12 +251,12 @@ NODE* insertNode(NODE* node, int newValue){
 /*  Função que faz a busca de elementos em árvore binária.
  *    
  *    Entradas: - node        ==> endereço do primeiro byte do pirmeiro parâmetro de uma
- *                             struct (tipo) node na qual se pretende fazer a busca
- *	          	- valueSearch ==> valor a ser buscado na árvore
+ *                                struct (tipo) node na qual se pretende fazer a busca
+ *	        - valueSearch ==> valor a ser buscado na árvore
  *     
- *    Saída:    - node     ==> endereço do primeiro byte do primeiro parâmetro da 
- *                             struct (tipo) node na qual fora encontrado o elemento
- *                             buscado                              
+ *    Saída:    - node        ==> endereço do primeiro byte do primeiro parâmetro da 
+ *                                struct (tipo) node na qual fora encontrado o elemento
+ *                                buscado                              
  */
 NODE* searchNode(NODE* node, int valueSearch){
   if (node == NULL){
@@ -331,18 +333,18 @@ int main (int argc, char* argv[]){
   
   //Alocar espaco na memoria stack
   int lengh;          //Variável responsável por armazenar a quantidade de elementos
-         		          //a serem organizados em uma árbove binária de busca
+         	      //a serem organizados em uma árbove binária de busca
   int lengh_search;   //Variável resposnável por armzenar a quantidade de elementos
-		                  //a serem buscados na árvore
+		      //a serem buscados na árvore
   int aux;            //Variável que armazenará em si os elementos a serem organizados
-		                  // em árvore
+		      // em árvore
   int* search_vector; //Vetor que armazenará em si os elementos a serem buscados
   
   NODE* root;         //Ponteiro (struct node) responsável pela contrução da árvore. 
-		                  //root aponta para o endereço de uma struct node (raiz da árvore). 
+		      //root aponta para o endereço de uma struct node (raiz da árvore). 
   
   NODE* search;	      //Ponteiro (struct node) responsável pela busca em árvore. 
-		                  //search aponta para o endereço de uma struct node.	
+		      //search aponta para o endereço de uma struct node.	
 
   
   scanf("%d", &lengh);                //Leitura da quantidade de elementos da árvore
